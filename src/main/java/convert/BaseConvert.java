de6889faefc36bef79eac84a5dd8e8c8ebecb4b5
HP <brahimval25@gmail.com>
2021-01-07 19:50:46 +0300

    initial commit

--- src/main/java/convert/BaseConvert.java
+++ src/main/java/convert/BaseConvert.java
@@ -1,4 +1,5 @@
 package convert;
 
 public class BaseConvert {
+
 }
--- src/main/java/model/BaseDTO.java
+++ src/main/java/model/BaseDTO.java
@@ -1,5 +1,18 @@
 package model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+@Getter
+@Setter
 public class BaseDTO {
+    private long id;
+    private long warehouseCode;
+    private  String name;
+    private Date creatDate;
+    private Date updateDate;
+
+
 
 }
